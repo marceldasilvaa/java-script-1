fba6a736e94bd1d869d4631faf61451222643172
Marcel Alves da Silva da Costa <122131624+marceldasilva@users.noreply.github.com>
2024-02-21 00:14:32 -0300

    Update ex003.js

---
+++ ex005/ex003.js
@@ -0,0 +1,21 @@
+var data = new Date()
+var diaSem = data.getDay()
+
+//console.log(diaSem)
+
+switch (diaSem) {
+    case 1: console.log('Domingo')
+        break
+    case 2: console.log('Segunda')
+        break
+    case 3: console.log('Terça')
+        break
+    case 4: console.log('Quarta')
+        break  
+    case 5: console.log('Quinta')
+        break
+    case 6: console.log('Sexta')
+        break
+    case 7: console.log('Sábado')
+        break          
+}
