420f1803dc440accbfc77cebf746d90c871d2bc4
Marcel Alves da Silva da Costa <122131624+marceldasilva@users.noreply.github.com>
2024-02-22 20:06:06 -0300

    um esta feito

--- ex006/modelo/script.js
+++ ex006/modelo/script.js
@@ -5,5 +5,15 @@ function carregar() {
     var hora = data.getHours()
 
     msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        img.src = 'imagens/manha.png'
+        document.body.style.background = '#E6B66E'
+    } else if (hora < 18) {
+        img.src = 'imagens/tarde.png'
+        document.body.style.background = '#A87A60'
+    } else {
+        img.src = 'imagens/noite.png'
+        document.body.style.background = '#007AA3'
+    }
 }
 
