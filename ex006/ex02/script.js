3bb07107f91e0613b7a0644e3be9c519f941403e
Marcel Alves da Silva da Costa <122131624+marceldasilva@users.noreply.github.com>
2024-02-22 23:05:42 -0300

    criando o segundo exercicio

--- ex006/ex02/script.js
+++ ex006/ex02/script.js
@@ -1,1 +1,14 @@
-alert('Olá!')
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.querySelector('.res')
+    
+    if (fano.value.length == 0 || fano > ano) {
+        alert('[ERRO] Verifique os dados e tente novamente!')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - fano.value
+        res.innerHTML = `Sua idade é ${idade}`
+    }
+}
