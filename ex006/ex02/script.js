a4c59c23d8584c16e1fc63a35ea6695a7ec9ceef
Marcel Alves da Silva da Costa <122131624+marceldasilva@users.noreply.github.com>
2024-02-24 01:06:52 -0300

    modificando

--- ex006/ex02/script.js
+++ ex006/ex02/script.js
@@ -1,14 +1,9 @@
 function verificar() {
     var data = new Date()
     var ano = data.getFullYear()
-    var fano = document.getElementById('txtano')
+    var yano = document.querySelector('#txtano')
     var res = document.querySelector('.res')
-    
-    if (fano.value.length == 0 || fano > ano) {
-        alert('[ERRO] Verifique os dados e tente novamente!')
-    } else {
-        var fsex = document.getElementsByName('radsex')
-        var idade = ano - fano.value
-        res.innerHTML = `Sua idade é ${idade}`
-    }
+    var idade = ano - yano.value
+
+    res.innerHTML = `Sua idade é ${idade}`
 }
