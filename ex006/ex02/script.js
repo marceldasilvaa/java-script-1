ef5510e9232e640e2d21fae23339ca3722d1fc92
Marcel Alves da Silva da Costa <122131624+marceldasilva@users.noreply.github.com>
2024-02-24 17:14:15 -0300

    Update script.js

--- ex006/ex02/script.js
+++ ex006/ex02/script.js
@@ -11,12 +11,43 @@ function verificar() {
         var sex = document.getElementsByName('radsex')
         var idade = ano - yano.value
         var genero = ''
-    }
-    if (sex[0].checked) {
-        genero = 'Homem'
-    } else {
-        genero = 'Mulher'
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
     }
 
+    if (sex[0].checked) {
+        genero = 'Homem'
+        if (idade >= 0 && idade < 12) {
+            //crianca
+            img.setAttribute('src', 'bebe-m.png')
+        } else if (idade < 20) {
+            //jovem 
+            img.setAttribute('src', 'jovem-m.png')
+        } else if (idade < 50) {
+            //adulto
+            img.setAttribute('src', 'adulto-m.png')
+        } else {
+            //idoso
+            img.setAttribute('src', 'idoso-f.png') 
+        }
+    } else {
+        genero = 'Mulher'
+        if (idade >= 0 && idade < 12) {
+            //criança
+            img.setAttribute('src', 'bebe-f.png')
+        } else if (idade < 20) {
+            //jovem 
+            img.setAttribute('src', 'jovem-f.png')
+        } else if (idade < 50) {
+            //adulta
+            img.setAttribute('src', 'adulto-f.png')
+        } else {
+            //idosa
+            img.setAttribute('src', 'idoso-f.png')        
+        }
+    }
+
+    res.style.textAlign = 'center'
     res.innerHTML = `Detectamos ${genero} de ${idade} anos.`
+    res.appendChild(img)
 }
