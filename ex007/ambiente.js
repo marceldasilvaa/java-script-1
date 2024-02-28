c119d94c8ccc45f25a3a1bce6a9ea8e7cd3ff9c6
Marcel Alves da Silva da Costa <122131624+marceldasilva@users.noreply.github.com>
2024-02-27 22:58:17 -0300

    aprendendo while, do..while e for

---
+++ ex007/ambiente.js
@@ -0,0 +1,21 @@
+/*
+var c = 1
+while (c <= 10) {
+    console.log(`Contando ${c}`)
+    c++
+}
+*/
+/*
+var s = 1
+do {
+    console.log(`contar ${s}`)
+    s++
+} while (s <= 10)
+*/
+
+var nota = 1
+
+while (nota < 6) {
+    console.log(`Reprovado! Sua nota foi ${nota}`)
+    nota++
+}
