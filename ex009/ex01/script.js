a5134845e6755bad39bd5d49ab2dc8c9c05e10a8
Marcel Alves da Silva da Costa <122131624+marceldasilva@users.noreply.github.com>
2024-03-04 23:00:51 -0300

    refazendo exercicio

--- ex009/ex01/script.js
+++ ex009/ex01/script.js
@@ -1,17 +1,8 @@
 function contar() {
-    var início = document.querySelector('#fini')
-    var fim = document.querySelector('#ffim')
-    var passo = document.querySelector('#fpas')
-    var res = document.querySelector('.res')
-
-    if (início.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-        res.innerHTML = 'Impossível contar!'
-    } else {
-        var i = Number(início.value)
-        var f = Number(fim.value)
-        var p = Number(passo.value)
-        for (var c = i; c <= f; c += p) {
-            res.innerHTML = `Contando: ${c}`
-        }
+    var ini = document.querySelector('#txti')
+    var fim = document.querySelector('#txtf')
+    var pas = document.querySelector('#txtp')
+    if (ini.value.length == 0 || fim.value.length == 0 || pas.value.length == 0) {
+        alert('[ERRO] Preencha todos os campos!')
     }
 }
