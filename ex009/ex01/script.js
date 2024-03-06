f23be19915ade7b79157973e4e241dbfdc7a435c
Marcel Alves da Silva da Costa <122131624+marceldasilva@users.noreply.github.com>
2024-03-05 22:07:21 -0300

    terminando exercicio 1

--- ex009/ex01/script.js
+++ ex009/ex01/script.js
@@ -2,7 +2,25 @@ function contar() {
     var ini = document.querySelector('#txti')
     var fim = document.querySelector('#txtf')
     var pas = document.querySelector('#txtp')
+    var res = document.querySelector('.res')
     if (ini.value.length == 0 || fim.value.length == 0 || pas.value.length == 0) {
         alert('[ERRO] Preencha todos os campos!')
-    }
+    } else if (pas.value == 0) {
+        alert('Passo deve ser preenchido, quando vazio, consideramos o valor de 1')
+        pas.value = '1'
+    } else {
+        res.innerHTML = 'Contando: '
+        var i = Number(ini.value)
+        var f = Number(fim.value)
+        var p = Number(pas.value)
+        if (i < f) {
+            for (var c = i; c <= f; c += p) {
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        } else {
+            for (var c = i; c >= f; c -= p) {
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        } 
+    } 
 }
