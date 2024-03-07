8677c9ce767097d180e745cd1d0ac95e5a371611
Marcel Alves da Silva da Costa <122131624+marceldasilva@users.noreply.github.com>
2024-03-06 21:41:55 -0300

    iniciando exercicio 2

--- ex009/ex02/script.js
+++ ex009/ex02/script.js
@@ -1,1 +1,13 @@
-alert('Olá!')
+function gerar() {
+    var num = document.querySelector('#txtf')
+    var n = Number(num.value)
+    var sel = document.querySelector('#txtt')
+    
+    if (num.value.length == 0) {
+        alert('[ERRO] Digite qualquer número!')
+    } else if (n >= 0) {
+        var opt = document.createElement('option')
+        opt.innerHTML = '1'
+        appendChild('option')
+    }
+}
