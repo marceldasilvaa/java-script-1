0ae9acc6c811cee722817d3f0eb546f30ac47d98
Marcel Alves da Silva da Costa <122131624+marceldasilva@users.noreply.github.com>
2024-03-08 23:10:44 -0300

    conseguindo fazer o exercicio

--- ex009/ex02/script.js
+++ ex009/ex02/script.js
@@ -1,13 +1,21 @@
 function gerar() {
     var num = document.querySelector('#txtf')
     var n = Number(num.value)
-    var sel = document.querySelector('#txtt')
     
     if (num.value.length == 0) {
         alert('[ERRO] Digite qualquer número!')
     } else if (n >= 0) {
-        var res = document.querySelector('.res')
-        var m = 1 || 10
-        res.innerHTML = `${m}`
+        var sel = document.querySelector('#txts')
+        var opt = document.createElement('option')
+        var x = ''
+        var r = Number(n * x)
+        //Primeiro option
+        if (sel.options[0].selected) {
+            sel.add(opt, sel.options[0])
+            opt.value = 'Um'
+            for (var x = '1'; n >= x; n = r) {
+                opt.text = `${n} x ${x} = ${r}`
+            }
+        }
     }
 }
