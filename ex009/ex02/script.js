dc162ac633432de2aaa7d1ff52fcb12b176ca24c
Marcel Alves da Silva da Costa <122131624+marceldasilva@users.noreply.github.com>
2024-03-09 19:32:07 -0300

    continuando exercicio

--- ex009/ex02/script.js
+++ ex009/ex02/script.js
@@ -8,12 +8,12 @@ function gerar() {
         var sel = document.querySelector('#txts')
         var opt = document.createElement('option')
         var x = ''
-        var r = Number(n * x)
+        var r = n.value * x.value
         //Primeiro option
         if (sel.options[0].selected) {
             sel.add(opt, sel.options[0])
             opt.value = 'Um'
-            for (var x = '1'; n >= x; n = r) {
+            for(var x = 1; n >= x; n = r) {
                 opt.text = `${n} x ${x} = ${r}`
             }
         }
