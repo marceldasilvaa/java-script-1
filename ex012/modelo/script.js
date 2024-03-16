890e680caf4cd7d216b0769b0c6b4d6c52a7edb6
Marcel Alves da Silva da Costa <122131624+marceldasilva@users.noreply.github.com>
2024-03-16 19:24:11 -0300

    fazendo o exercicio

--- ex012/modelo/script.js
+++ ex012/modelo/script.js
@@ -2,14 +2,21 @@ let num = document.querySelector('#txtn')
 
 function adc() {
     if (num.value.length == 0) {
+        alert ('Valor inválido ou já encontrado na lista.')
+    }
+    else if (num.value > 100 || num.value < 1) {
         alert('Valor inválido ou já encontrado na lista.')
     } else {
-        let n = Number(num.value)
+        let n = [Number(num.value)]
         let sel = document.querySelector('#self')
-        sel.innerHTML = ''
         let opt = document.createElement('option')
-        opt.text = `O número digitado foi ${n}`
+        opt.text = `O número digitado foi ${n}.`
         opt.value = 'v1'
         sel.appendChild(opt)
-    }
+    } 
+}
+
+function final() {
+    let res = document.querySelector('.res')
+    res.innerHTML = 'Clicou!'
 }
