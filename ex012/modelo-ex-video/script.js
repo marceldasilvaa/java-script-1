86509079f5de61dad97d926d50de0f62619a8f2b
Marcel Alves da Silva da Costa <122131624+marceldasilva@users.noreply.github.com>
2024-03-23 17:40:38 -0300

    exercicio feito

--- ex012/modelo-ex-video/script.js
+++ ex012/modelo-ex-video/script.js
@@ -6,9 +6,15 @@ let valores = []
 function adicionar() {
     if (isNumero(num.value) && !inList(num.value, valores)) {
         valores.push(Number(num.value))
+        let opt = document.createElement('option')
+        opt.text = `Valor ${num.value} cadastrado.`
+        lista.appendChild(opt)
+        res.innerHTML = ''
     } else {
        alert('Valor inválido ou já adicionado na lista.')
     }
+    num.value = ''
+    num.focus()
 }
 
 function isNumero(n) {
@@ -24,5 +30,31 @@ function inList(n, v) {
         return true
     } else {
         return false
+    }
+}
+
+function finalizar() {
+    if (valores.length == 0) {
+        alert('Adicione valores antes de finalizar.')
+    } else {
+        let tot = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+        for(let pos in valores) {
+            soma += valores[pos]
+            if (valores[pos] > maior)
+                maior = valores[pos]
+            if (valores[pos] < menor)
+                menor = valores[pos]
+        }
+        media = soma / tot
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo, temos ${tot} números cadastrados.</p>`
+        res.innerHTML += `<p>O maior valor informado foi ${maior}.</p>`
+        res.innerHTML += `<p>O menor valor informado foi ${menor}.</p>`
+        res.innerHTML += `<p>Somando os valores, temos ${soma}.</p>`
+        res.innerHTML += `<p>A média dos valores digitados é de ${media}.</p>`
     }
 }
