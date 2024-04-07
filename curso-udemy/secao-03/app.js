e1a6c76507ae0ab42128199a2725302c7341f18a
Marcel Alves da Silva da Costa <122131624+marceldasilva@users.noreply.github.com>
2024-04-07 11:05:39 -0300

    começando

---
+++ curso-udemy/secao-03/app.js
@@ -0,0 +1,10 @@
+let nome = "Manuel"
+let sobrenome = "Silva"
+let idade = 24
+const numCliente = 294057
+
+let valorEmprestimo = 150000
+let taxaDeJuros = 10
+let ehBomPagador = true
+
+console.log("Boa tarde Sr. " + nome + " " + sobrenome + ". " + "Sua idade é: " + idade)
