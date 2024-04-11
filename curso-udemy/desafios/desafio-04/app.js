74e95b7f5c2f89752b3fbaa747c3c8777689ba2b
Marcel Alves da Silva da Costa <122131624+marceldasilva@users.noreply.github.com>
2024-04-10 23:24:30 -0300

    terminando mais desafios

--- curso-udemy/desafios/desafio-04/app.js
+++ curso-udemy/desafios/desafio-04/app.js
@@ -1,6 +1,7 @@
-let quantidade
-let preco = 1.99
+let quantidade = 1
+let preco = 0
 
+/*
 do {
     quantidade = parseInt(prompt('Digite a quantidade de itens'))
     for (let c = 1; c < quantidade; c++) {
@@ -8,3 +9,17 @@ do {
     }
     console.log(preco)
 } while (preco < quantidade)
+*/
+
+for (let c = 50; quantidade <= c; quantidade++) {
+    preco += 1.99
+    console.log(quantidade)
+    console.log(preco)
+}
+
+// Resolução da aula
+
+console.log('LOJA QUASE DOIS - TABELA DE PREÇOS')
+for (let i = 1; i <= 50; i++) {
+    console.log(`${i} - R$ ${i * 1.99}`)
+}
