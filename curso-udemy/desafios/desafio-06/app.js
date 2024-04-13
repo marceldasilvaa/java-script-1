9c32c8bf974cd17e9a98f2324148139a6b86c326
Marcel Alves da Silva da Costa <122131624+marceldasilva@users.noreply.github.com>
2024-04-13 16:43:30 -0300

    Update app.js

--- curso-udemy/desafios/desafio-06/app.js
+++ curso-udemy/desafios/desafio-06/app.js
@@ -1,13 +1,3 @@
-let questao1
-let questao2
-let questao3
-let questao4
-let questao5
-
-function investigacao(questao1, questao2, questao3, questao4, questao5) {
-    questao1 = prompt('Você telefonou para a vítima?')
-    questao2 = prompt('Você esteve no local do crime?')
-    questao3 = prompt('Você mora perto da vítima?')
-    questao4 = prompt('Você devia para a vítima?')
-    questao5 = prompt('Você já trabalhou com a vítima?')
+function investigacao() {
+    let pergunta1 = prompt('Você telefonou para a vítima?')
 }
