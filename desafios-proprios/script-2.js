e6bf67c2b0e15c372bdfcbd0a6f13f44c05bb426
Marcel Alves da Silva da Costa <122131624+marceldasilva@users.noreply.github.com>
2024-05-06 22:47:24 -0300

    Update script-2.js

--- desafios-proprios/script-2.js
+++ desafios-proprios/script-2.js
@@ -1,3 +1,18 @@
 function validaCadastro(nome, idade, salario, sexo, estadoCivil) {
-       
+    for (const letra of nome) {
+        if(nome.length > 3) {
+            return nome;
+        } else {
+            return 'Cadastro invalido';
+        }
+    }
+    for (const numero in idade) {
+        if (idade < 0 && idade > 150) {
+            return 'Cadastro invalido';
+        } else {
+            return idade;
+        }
+    }
 }
+
+console.log(validaCadastro('Marcel', 19));
