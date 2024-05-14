c782498aebe28e63ee04c7f7cc80ffac5bf8c0f8
Marcel Alves da Silva da Costa <122131624+marceldasilva@users.noreply.github.com>
2024-05-13 23:03:42 -0300

    Create app.js

---
+++ curso-udemy/secoes/secao-28/app.js
@@ -0,0 +1,17 @@
+/*
+try {
+    let x = y + 10;
+    console.log(x);
+} catch (erro) {
+    console.log(erro);
+    console.log(erro.name);
+}
+*/
+
+try {
+    let num = 1;
+    num.toUpperCase();
+} catch (erro) {
+    console.log(erro);
+    console.log(erro.name);
+}
