68c1adaa46256aa803098809b4a3b2de8db253d7
Marcel Alves da Silva da Costa <122131624+marceldasilva@users.noreply.github.com>
2024-08-01 23:31:01 -0300

    fazendo outro uso de api com async / await

--- curso-udemy/projetos/projeto-info-filmes/app.js
+++ curso-udemy/projetos/projeto-info-filmes/app.js
@@ -1,8 +1,50 @@
-const apiKey = "c4f07ff4";
+const nomeBusca = document.querySelector(".input")
+const msgErro = document.querySelector(".msgErro")
+const botaoBusca = document.querySelector("#botao-busca")
+const poster = document.querySelector(".poster")
+const titulo = document.querySelector("#titulo")
+const sinopse = document.querySelector("#sinopse")
+const ano = document.querySelector("#ano")
+const duracao = document.querySelector("#duracao")
+const genero = document.querySelector("#genero")
+const atores = document.querySelector("#atores")
+const diretor = document.querySelector("#diretor")
+const apiKey = "49eaa790";
 const imgDefault = "./default_image.png";
 
 async function buscaFilme(nomeBusca){
-     const resposta = await fetch(`http://www.omdbapi.com/?t=${nomeBusca}&apikey=${apiKey}`);
+     const resposta = await fetch(`http://www.omdbapi.com/?i=${nomeBusca}&apikey=${apiKey}`);
      return resposta.json();
 }
 
+botaoBusca.addEventListener("click", () => {
+     limparCampos()
+     core()
+})
+
+async function core() {
+     const filme = await buscaFilme(nomeBusca.value)
+     defineValores(filme)
+}
+
+function defineValores(filme) {
+     titulo.textContent = filme.Title
+     sinopse.textContent = filme.Plot
+     genero.textContent = `Genre: ${filme.Genre}`
+     ano.textContent = `Year: ${filme.Year}`
+     duracao.textContent = `Run Time: ${filme.Runtime}`
+     atores.textContent = `Actors: ${filme.Actors}`
+     diretor.textContent = `Director: ${filme.Director}`
+     poster.setAttribute("src", filme.Poster)
+}
+
+function limparCampos() {
+     titulo.textContent = ""
+     sinopse.textContent = ""
+     genero.textContent = ""
+     ano.textContent = ""
+     duracao.textContent = ""
+     atores.textContent = ""
+     diretor.textContent = ""
+     poster.setAttribute("src", imgDefault)
+}
