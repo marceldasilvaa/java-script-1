091d3789eb884fa723c37302cd27fae7d8a6b86f
Marcel <marcelalvessc@gmail.com>
2024-09-03 11:24:35 -0300

    mais sobre react

---
+++ curso-react/2-componentes/componentes/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import FirstComponent from "./components/FirstComponents.js";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Fundamentos React</h1>
+      <FirstComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ curso-react/2-componentes/componentes/src/components/FirstComponents.js
@@ -0,0 +1,14 @@
+//components
+const FirstComponent = () => {
+    
+    // essa função faz isso
+    return (
+        <div>
+            {/*Algum comentário*/}
+            <h1>Meu primeiro componente</h1>
+            <div className="teste" >Meu texto</div>
+        </div>
+    );
+}
+
+export default FirstComponent;
