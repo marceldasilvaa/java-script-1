bccd017cfbb1e8b27c2a2d73e620d8ff529fbfe9
Marcel <marcelalvessc@gmail.com>
2024-09-06 22:11:07 -0300

    projeto veterinaria

--- desafios-proprios/veterinaria/veterinaria.js
+++ desafios-proprios/veterinaria/veterinaria.js
@@ -1,6 +1,5 @@
-const nome = document.getElementById("nomeAnimal");
-const idade = document.getElementById("idadeAnimal");
 const especie = document.getElementById("especieAnimal");
+const idade = document.getElementById("idadeAnimal");
 const raca = document.getElementById("racaAnimal");
 const sexo = document.getElementById("sexoAnimal");
 const botaoAgenda = document.querySelector(".botaoAgenda");
