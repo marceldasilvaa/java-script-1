985d92d6ca9da803c8eff1e87734f6157ffe9ded
Marcel <marcelalvessc@gmail.com>
2024-10-02 18:06:50 -0300

    forms em react

---
+++ curso-react/5-form-em-react/form/src/components/MyForm.js
@@ -0,0 +1,34 @@
+import styles from "./MyForm.css";
+import {useState} from "react";
+
+const MyForm = () => {
+    // 3 - gerenciamento de dados
+    const [name, setName] = useState();
+    const [email, setEmail] = useState();
+
+    const handleName = (e) => {
+        setName(e.target.value);
+    }
+    
+    console.log(name);
+
+  return (
+    <div>
+      {/* criacao de form */}
+      <form>
+        <div>
+            <label htmlFor="name">Nome:</label>
+            <input type="text" name="name" placeholder="Digite seu nome aqui" onChange={handleName}/>
+        </div>
+        {/* label envolvendo input */}
+        <label>
+            <span>Email:</span>
+            <input type="text" name="email" placeholder="Digite seu email aqui" />
+        </label>
+        <input type="submit" value="Enviar" />
+      </form>
+    </div>
+  )
+}
+
+export default MyForm
