91967b77e435e9d3e01bf6f26645d40b6a3b8c31
Marcel <marcelalvessc@gmail.com>
2024-10-25 12:07:19 -0300

    colocando CSS no game

--- curso-react/6-secret-word/secret-word/src/components/Game.js
+++ curso-react/6-secret-word/secret-word/src/components/Game.js
@@ -10,6 +10,7 @@ const Game = ({verifyLetter}) => {
       <h3 className="tip">
         Dica sobre a palavra: <span>Dica... </span>
       </h3>
+      <p>Você ainda tem XXX tentativas</p>
       <div className="wordContainer">
         <span className="letter">A</span>
         <span className="blankSquare"></span>
