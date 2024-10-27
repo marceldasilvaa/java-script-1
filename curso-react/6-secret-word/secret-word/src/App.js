67875bf79383f8b7da88cce5737b3e00c364ac7f
Marcel <marcelalvessc@gmail.com>
2024-10-27 00:27:11 -0300

    continuando no projeto

--- curso-react/6-secret-word/secret-word/src/App.js
+++ curso-react/6-secret-word/secret-word/src/App.js
@@ -25,6 +25,10 @@ function App() {
   const [pickedWord, setPickedWord] = useState("");
   const [pickedCategory, setPickedCategory] = useState("");
   const [letters, setLetters] = useState("");
+  const [guessedLetters, setGuessedLetters] = useState([]);
+  const [wrongLetters, setWrongLetters] = useState([]);
+  const [guesses, setGuesses] = useState(3);
+  const [score, setScore] = useState(0);
 
   const pickWordAndCategory = () => {
     // pick a random category
@@ -55,7 +59,7 @@ function App() {
     // fill state
     setPickedWord(word);
     setPickedCategory(category);
-    setLetters(letters);
+    setLetters(wordLetters);
 
     setGameStage(stages[1].name);
   }
@@ -72,7 +76,17 @@ function App() {
   return (
     <div className="App">
       {gameStage === 'start' && <StartScreen startGame={startGame} />}
-      {gameStage === 'game' && <Game verifyLetter={verifyLetter} />}
+      {gameStage === 'game' && 
+        <Game
+          verifyLetter={verifyLetter} 
+          pickedWord={pickedWord}
+          pickedCategory={pickedCategory}
+          letters={letters}
+          guessedLetters={guessedLetters}
+          wrongLetters={wrongLetters}
+          guesses={guesses}
+          score={score}
+        />}
       {gameStage === 'end' && <GameOver retry={retry} />}
     </div>
   );
