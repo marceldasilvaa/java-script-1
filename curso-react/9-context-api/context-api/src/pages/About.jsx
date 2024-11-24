47a7b16effd71779e6bf034dc57eee91a4552333
Marcel <marcelalvessc@gmail.com>
2024-11-23 21:29:26 -0300

    terminando o context react

--- curso-react/9-context-api/context-api/src/pages/About.jsx
+++ curso-react/9-context-api/context-api/src/pages/About.jsx
@@ -1,15 +1,19 @@
 // import { useContext } from "react";
 // import { CounterContext } from "../context/CounterContext";
+
 import { useCounterContext } from "../hooks/useCounterContext";
+import { useTitleColorContext } from "../hooks/useTitleColorContext";
 
 const About = () => {
   // const { counter } = useContext(CounterContext);
+
+  const { color } = useTitleColorContext();
 
   const { counter } = useCounterContext();
 
   return (
     <div>
-      <h1>Sobre</h1>
+      <h1 style={{ color: color }}>Sobre</h1>
       <p>Olá, esta é a segunda página do site</p>
       <p>Valor do contador: {counter}</p>
     </div>
--- curso-react/9-context-api/context-api/src/pages/Home.jsx
+++ curso-react/9-context-api/context-api/src/pages/Home.jsx
@@ -11,14 +11,28 @@ import { useTitleColorContext } from "../hooks/useTitleColorContext";
 const Home = () => {
   // const { counter } = useContext(CounterContext);
 
+  // 5 - context mais complexo
+  const { color, dispatch } = useTitleColorContext();
+
+  // 6 - alterando context complexo
+  const setTitleColor = (color) => {
+    dispatch({ type: color });
+  };
+
   const { counter } = useCounterContext();
 
   return (
     <div>
-      <h1>Home</h1>
+      <h1 style={{ color: color }}>Home</h1>
       <p>Olá, esta é a página principal do site</p>
       <p>Valor do contador: {counter}</p>
       <ChangeCounter />
+      {/* 6 - alterando context complexo */}
+      <div>
+        <button onClick={() => setTitleColor("RED")}>Vermelho</button>
+        <button onClick={() => setTitleColor("YELLOW")}>Amarelo</button>
+        <button onClick={() => setTitleColor("BLUE")}>Azul</button>
+      </div>
     </div>
   );
 };
