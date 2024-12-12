35f8d2439afa646401f8b44dc5b55f4e9bc3333f
Marcel <marcelalvessc@gmail.com>
2024-12-11 22:16:39 -0300

    configurando a página home

--- curso-react/10-miniblog/miniblog/src/pages/Home/Home.jsx
+++ curso-react/10-miniblog/miniblog/src/pages/Home/Home.jsx
@@ -1,10 +1,43 @@
 // CSS
 import styles from "./Home.module.css";
 
+// hooks
+import { useNavigate, Link } from "react-router-dom";
+import { useState } from "react";
+
+// components
+
 const Home = () => {
+  const [query, setQuery] = useState("");
+  const [posts] = useState([]);
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div>
-      <h1>Home</h1>
+    <div className={styles.home}>
+      <h1>Veja os nossos posts mais recentes</h1>
+      <form onSubmit={handleSubmit} className={styles.searchForm}>
+        <input
+          type="text"
+          placeholder="Ou busque por tags..."
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button className="btn btnDark">Pesquisar</button>
+      </form>
+      <div>
+        <h1>Posts...</h1>
+        {posts && posts.length === 0 && (
+          <div className={styles.noposts}>
+            <p>Não foram encotrados posts...</p>
+            <Link to="/posts/create" className="btn">
+              Criar primeiro post
+            </Link>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
