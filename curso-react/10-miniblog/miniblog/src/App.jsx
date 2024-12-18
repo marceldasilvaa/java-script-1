556fb1fd85460c92e6ad5dd22b2575f8ae0eafe7
Marcel <marcelalvessc@gmail.com>
2024-12-17 23:19:18 -0300

    criando pagina para post com id proprio

--- curso-react/10-miniblog/miniblog/src/App.jsx
+++ curso-react/10-miniblog/miniblog/src/App.jsx
@@ -24,6 +24,7 @@ import Login from "./pages/Login/Login";
 import CreatePost from "./pages/CreatePost/CreatePost";
 import Dashboard from "./pages/Dashboard/Dashboard";
 import Search from "./pages/Search/Search";
+import Post from "./pages/Post/Post";
 
 function App() {
   const [user, setUser] = useState(undefined);
@@ -52,6 +53,7 @@ function App() {
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
                 <Route path="/search" element={<Search />} />
+                <Route path="/posts/:id" element={<Post />} />
                 <Route
                   path="/register"
                   element={!user ? <Register /> : <Navigate to="/" />}
