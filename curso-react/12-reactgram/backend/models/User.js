6903df5fed64573864e215dcf31bc2313d3d2a25
Marcel <marcelalvessc@gmail.com>
2025-01-14 22:55:30 -0300

    criando o database e os modules de usuário e foto

---
+++ curso-react/12-reactgram/backend/models/User.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const userSchema = new Schema(
+  {
+    nome: String,
+    email: String,
+    password: String,
+    profileImage: String,
+    bio: String,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
