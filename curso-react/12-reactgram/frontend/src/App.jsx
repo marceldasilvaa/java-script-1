c753b282fb66bfa3f7eccc9739ac001029af9397
Marcel <marcelalvessc@gmail.com>
2025-01-22 10:44:56 -0300

    primeiras paginas

--- curso-react/12-reactgram/frontend/src/App.jsx
+++ curso-react/12-reactgram/frontend/src/App.jsx
@@ -1,14 +1,25 @@
-import './App.css'
+import "./App.css";
+
+// pages
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Login from "./pages/Auth/Login";
+import Register from "./pages/Auth/Register";
 
 function App() {
-
   return (
     <>
       <div>
-        <h1>ReactGram</h1>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </BrowserRouter>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
