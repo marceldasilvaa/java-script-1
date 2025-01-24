b8b7fc048c54f087db3cf9b8b13ebe57105a2113
Marcel <marcelalvessc@gmail.com>
2025-01-24 12:19:36 -0300

    fazendo conexão com o banco através do redux

--- curso-react/12-reactgram/frontend/src/pages/Auth/Register.jsx
+++ curso-react/12-reactgram/frontend/src/pages/Auth/Register.jsx
@@ -5,12 +5,20 @@ import { Link } from "react-router-dom";
 
 // hooks
 import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+// redux
+import { register, reset } from "../../slices/AuthSlice";
 
 const Register = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
+
+  const dispatch = useDispatch();
+
+  const { loading, error } = useSelector((state) => state.auth);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -23,7 +31,14 @@ const Register = () => {
     };
 
     console.log(user);
+
+    dispatch(register(user));
   };
+
+  // clan all auth states
+  useEffect(() => {
+    dispatch(reset());
+  }, [dispatch])
 
   return (
     <div id="register">
