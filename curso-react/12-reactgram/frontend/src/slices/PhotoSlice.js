abb56a256cf9870529eb5a4e03deba48e452e49d
Marcel <marcelalvessc@gmail.com>
2025-02-04 16:46:07 -0300

    implementando o search e fianlizando o projeto do curso

--- curso-react/12-reactgram/frontend/src/slices/PhotoSlice.js
+++ curso-react/12-reactgram/frontend/src/slices/PhotoSlice.js
@@ -125,13 +125,28 @@ export const comment = createAsyncThunk(
 );
 
 // get all photos
-export const getPhotos = createAsyncThunk("photo/getall", async (_, thunkAPI) => {
-  const token = thunkAPI.getState().auth.user.token;
+export const getPhotos = createAsyncThunk(
+  "photo/getall",
+  async (_, thunkAPI) => {
+    const token = thunkAPI.getState().auth.user.token;
 
-  const data = await photoService.getPhotos(token);
+    const data = await photoService.getPhotos(token);
 
-  return data;
-});
+    return data;
+  }
+);
+
+// search photo by query
+export const searchPhotos = createAsyncThunk(
+  "photo/search",
+  async (query, thunkAPI) => {
+    const token = thunkAPI.getState().auth.user.token;
+
+    const data = await photoService.searchPhotos(query, token);
+
+    return data;
+  }
+);
 
 export const photoSlice = createSlice({
   name: "photo",
@@ -261,6 +276,16 @@ export const photoSlice = createSlice({
         state.error = false;
       })
       .addCase(getPhotos.fulfilled, (state, action) => {
+        state.loading = false;
+        state.success = true;
+        state.error = null;
+        state.photos = action.payload;
+      })
+      .addCase(searchPhotos.pending, (state) => {
+        state.loading = true;
+        state.error = false;
+      })
+      .addCase(searchPhotos.fulfilled, (state, action) => {
         state.loading = false;
         state.success = true;
         state.error = null;
